7b891119db0b583f1b9ea2938facf53618cbec4a
IBUNHABIBU <ibunhabib@gmail.com>
2024-06-18 18:14:55 +0300

    Implement connect function in geolocation controller

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -5,6 +5,10 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ["button"];
 
+  connect() {
+    this.updateCoordinates();
+  }
+
   updateCoordinates() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
