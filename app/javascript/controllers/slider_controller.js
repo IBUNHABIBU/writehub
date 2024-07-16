27e0b635af22575ef61e7e3a17598a1194138cc2
IBUNHABIBU <ibunhabib@gmail.com>
2024-07-16 13:55:42 +0300

    Add carousel display

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,53 +1,50 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = [ "dot" ]
-    static values = { index: Number }
+  static targets = ["dot"];
+  static values = { index: Number }
 
-    connect() {
-        this.showSlide()
-        this.startAutoSlide()
-        console.log("Slider connected")
-    }
+  connect() {
+    this.indexValue = 0;
+    this.showSlide();
+    this.startAutoSlide();
+  }
 
-    disconnect() {
-        this.stopAutoSlide()
-    }
+  disconnect() {
+    this.stopAutoSlide();
+  }
 
-    startAutoSlide() {
-        this.interval = setInterval(() => {
-            this.nextSlide()
-        }, 3000)
-    }
+  startAutoSlide() {
+    this.interval = setInterval(() => {
+      this.nextSlide();
+    }, 5000); // Change slide every 5 seconds
+  }
 
-    stopAutoSlide() {
-        clearInterval(this.interval)
-    }
+  stopAutoSlide() {
+    clearInterval(this.interval);
+  }
 
-    showSlide() {
-        const sliders = this.element.querySelectorAll('.slider__bg');
-        let i = 0;
-    
-        setInterval(() => {
-          sliders.forEach((slider, index) => {
-            slider.style.display = 'none';
-            if (index === i % sliders.length) {
-              slider.style.display = 'block';
-            }
-          });
-          i++;
-        }, 10000);
-    }
+  showSlide(index = this.indexValue) {
+    const sliders = this.element.querySelectorAll('.slider__bg');
+    sliders.forEach((slider, i) => {
+      slider.style.display = i === index ? 'block' : 'none';
+    });
+    this.dotTargets.forEach((dot, i) => {
+      dot.classList.toggle('active', i === index);
+    });
+    this.indexValue = index;
+  }
 
-    nextSlide() {
-        let nextIndex = (this.indexValue + 1) % this.dotTargets.length;
-        this.showSlide(nextIndex);
-    }
+  nextSlide() {
+    this.indexValue = (this.indexValue + 1) % this.dotTargets.length;
+    this.showSlide();
+  }
 
-    gotoSlide(event) {
-       const index = parseInt(event.target.dataset.index);
-       this.showSlide(index);
-       this.stopAutoSlide();
-       this.startAutoSlide();
-    }
+  goToSlide(event) {
+    const index = parseInt(event.currentTarget.dataset.index);
+    this.stopAutoSlide();
+    this.showSlide(index);
+    this.startAutoSlide();
+  }
 }
+
