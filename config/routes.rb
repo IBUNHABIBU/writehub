44e185a49118d93ef849f186b61104972be9bef4
IBUNHABIBU <ibunhabib@gmail.com>
2024-07-24 14:32:14 +0300

    Now I have managed to update the coordinates and save it to the session

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
 
   post '/update_coordinates', to: 'locations#update_coordinates'
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
