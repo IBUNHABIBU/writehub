7206ace84b2ffeaa2ef65a54b96d4881d34f8d36
IBUNHABIBU <ibunhabib@gmail.com>
2024-08-07 22:29:15 +0300

    Add weather icon

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -8,11 +8,13 @@ class WeatherService
     weather_data = fetch_weather_data(latitude, longitude)
     city_name = fetch_city_name(latitude, longitude)
     city_image = fetch_city_image(city_name)
+    weather_icon = fetch_weather_icon(weather_data)
 
     {
       weather_data: weather_data,
       city_name: city_name,
-      city_image: city_image
+      city_image: city_image,
+      weather_icon: weather_icon
     }
   end
 
@@ -33,5 +35,10 @@ class WeatherService
     client = Pexels::Client.new(PEXELS_API_KEY)
     city_response = client.photos.search(city_name,page: 1, per_page: 1)
     city_response.photos[0].src["original"]
+  end
+
+  def self.fetch_weather_icon(weather_data)
+    icon = weather_data.dig("weather", 0, "icon")
+    "https://openweathermap.org/img/wn/#{icon}@2x.png"
   end
 end
