087f3b53f9a47e47c4943c21a47011fa01b2b571
IBUNHABIBU <ibunhabib@gmail.com>
2024-08-08 00:01:22 +0300

    Style the weather

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -7,14 +7,17 @@ class WeatherService
   def self.fetch_weather_and_image(latitude, longitude)
     weather_data = fetch_weather_data(latitude, longitude)
     city_name = fetch_city_name(latitude, longitude)
+    city_info = fetch_city_name(latitude, longitude)
     city_image = fetch_city_image(city_name)
     weather_icon = fetch_weather_icon(weather_data)
 
     {
       weather_data: weather_data,
       city_name: city_name,
+      country_name: city_info[:country],
       city_image: city_image,
-      weather_icon: weather_icon
+      weather_icon: weather_icon,
+      
     }
   end
 
@@ -28,7 +31,9 @@ class WeatherService
   def self.fetch_city_name(latitude, longitude)
     result = Geocoder.search([latitude, longitude]).first
     # logger.info "Service: #{result}"
-    result&.city || "Unknown City"
+    city = result&.city || "Unknown City"
+    country = result&.country || "Unknown Country"
+    { city: city, country: country }
   end
 
   def self.fetch_city_image(city_name)
