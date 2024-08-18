4968f89e4ebb26def5f1de4d7a3528f893475250
IBUNHABIBU <ibunhabib@gmail.com>
2024-08-18 12:19:42 +0300

    Add credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :branch, ENV['BRANCH'] || 'main'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/master.key'
+# append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
