bb1ab7189c855b6f3d2a2180a3d9051213f30483
IBUNHABIBU <ibunhabib@gmail.com>
2024-09-20 22:29:34 +0300

    fix credentials

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Railstarter
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,14 +23,5 @@ module Railstarter
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    Geocoder.configure(
-      timeout: 5,                 # geocoding service timeout (secs)
-      # lookup: :ipinfo_io, 
-      lookup: :google, 
-      api_key: Rails.application.credentials.google_api,
-      use_https: true,
-      units: :km
-    )
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,15 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-# Rails.logger = Logger.new(Rails.root.join("log", Rails.env + ".log"))
-Rails.logger = Logger.new(("log/#{Rails.env}.log"))
-Rails.logger.level = Logger::DEBUG
-Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
-
-Rails.logger.formatter = proc do |severity, datetime, progname, msg|
-    "#{datetime} | #{severity} | #{progname} | #{msg}"
-end 
-
-Rails.logger.info("Environment: #{Rails.env}") do 
-    "Rails logger started"
-end
