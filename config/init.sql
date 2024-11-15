d11147c5a8264c706b0e333e063e6c39271e1760
IBUNHABIBU <ibunhabib@gmail.com>
2024-11-15 14:40:06 +0300

    Add db configurations

--- config/init.sql
+++ config/init.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE linodeblog;
+DO
+$$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname='writehub_production') THEN
+      CREATE DATABASE writehub_production;
+    END IF;
+END
+$$ LANGUAGE plpgsql;
