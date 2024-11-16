0fc29b86a02bc5284d933041a5f1c6503e6edc2a
IBUNHABIBU <ibunhabib@gmail.com>
2024-11-16 13:22:15 +0300

    Add geocoder

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,10 @@ class ArticlesController < ApplicationController
     @recent_articles = Article.recent
     @most_rated_articles = Article.most_rated
     @weather_info = WeatherService.fetch_weather_and_image(@coordinates[0], @coordinates[1])
+    
+
+    Rails.logger.debug(":  Weather info ***************** #{Geocoder} , #{@weather_info} *************")
+    
   end
 
    # GET /articles/1
