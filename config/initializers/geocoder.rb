de863e247dc20c3487c11d0d9691d1368f33fc6e
IBUNHABIBU <ibunhabib@gmail.com>
2024-11-16 08:55:44 +0300

    Add geocoder credentials

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,6 +3,7 @@ Geocoder.configure(
     lookup: google,
     api_key: ENV['GOOGLE_API'],
     ip_lookup: :ipinfo_io,    
-    units: :km,              
+    units: :km,           
+    use_https: true,   
     http_headers: { 'Referer' => 'https://writehub.cyou' }
 )
