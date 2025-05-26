062ef096134d6590db7c2fe898cdf93deb883f31
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-26 11:51:52 +0300

    Add user location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_09_20_191524) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_26_085116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -73,6 +73,14 @@ ActiveRecord::Schema[8.0].define(version: 2024_09_20_191524) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_likes_on_article_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "user_locations", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
